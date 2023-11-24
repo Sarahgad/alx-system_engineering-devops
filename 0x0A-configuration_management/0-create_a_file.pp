0b567491dd09505a4a04cacf0696f85152e49dc1
Sarahgad <sarahgad91@gmail.com>
2023-11-24 11:26:57 +0200

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 #create a file in /tmp.
-file { '/tmp/school':
-owner   => 'sarahgad',
-group   => 'wheel',
+file { 'school':
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
 mode    => '0744',
 content => 'I love Puppet',
 }
