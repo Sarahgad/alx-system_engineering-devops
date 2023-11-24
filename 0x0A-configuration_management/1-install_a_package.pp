eaea2147b2e88514efd26d0e2cfd4224a859eb2e
Sarahgad <sarahgad91@gmail.com>
2023-11-24 13:17:42 +0200

    2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
-#install flask
-package {'flask':
-  ensure   =>'2.1.0',
-  provider => 'pip3',
-  require  => 'Werkzeug 2.1.1'
+# #install flask
+# package {'flask':
+#   ensure   =>'2.1.0',
+#   provider => 'pip3',
+# }
+# install flask from pip3 using Puppet
+exec {'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0'
 }
