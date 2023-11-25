b6c2cdeb4ba0cc36b06411d6f3dc27ab7bf33598
Sarahgad <sarahgad91@gmail.com>
2023-11-26 01:22:08 +0200

    10

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # configure_ssh.pp
 
 augeas { 'ssh_identity':
-  context => '/etc/ssh/ssh_config', # Replace with the actual path
+  context => '~/.ssh/school', # Replace with the actual path
   changes => [
     'set Host[1]/IdentityFile[1] ~/.ssh/school',
     'set Host[1]/PasswordAuthentication no',
