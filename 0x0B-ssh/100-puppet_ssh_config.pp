eafa9ae8e1f719c38384df477608744b9c272714
Sarahgad <sarahgad91@gmail.com>
2023-11-26 00:15:50 +0200

    100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+#enshure file config
+file_line {'.ssh/config':
+  path => '~/.ssh/config,
+  line => 'PubkeyAuthentication yes',
+  line =>	'PasswordAuthentication no',
+	line => 'IdentityFile ~/.ssh/school'
+  }
