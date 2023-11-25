fa9de74818937a6566a4e23a57e82ceb9d2be697
Sarahgad <sarahgad91@gmail.com>
2023-11-26 00:47:46 +0200

    0

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,26 +2,23 @@
 include stdlib
 
 file_line {'Authentication':
-  enshure =>  'absent',
+  enshure =>  'present',
   path    =>  '~/.ssh/config',
   line    =>  ' PubkeyAuthentication yes',
-  match   =>  ' PubkeyAuthentication yes',
   replace =>  true
 }
 
 file_line { 'PasswordAuthentication':
-  enshure =>  'absent',
+  enshure =>  'present',
   path    =>  '~/.ssh/config',
   line    =>  ' PasswordAuthentication no',
-  match   =>  ' PasswordAuthentication no',
   replace =>  true
 
 }
 
 file_line {'IdentityFile':
-  enshure =>  'absent',
+  enshure =>  'present',
   path    => '~/.ssh/config',
   line    => ' IdentityFile ~/.ssh/school',
-  match   => ' IdentityFile ~/.ssh/school',
   replace =>  true
 }
