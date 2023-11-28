f750f42ae3a0c0d91bc9c400e0e5cf9e462557b8
Sarahgad <sarahgad91@gmail.com>
2023-11-29 00:58:13 +0200

    88

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,3 @@
-# # Set resource defaults
-# Package { ensure => 'present' }
-# File { ensure => 'present', owner => 'ubuntu', group => 'ubuntu', mode => '0644' }
-# Service { ensure => 'running', enable => true }
-
-# # Install nginx package
-# package { 'nginx': }
-
-# # Create the index file with dynamic content
-# file { '/var/www/html/index.nginx-debian.html':
-#   content => 'Hello, World!',
-# }
-
-# # Configure nginx with the redirect
-# exec { 'configure_nginx':
-#   command => '/bin/sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
-#   require => Package['nginx'],
-#   notify  => Service['nginx'],
-# }
-
-# # Restart nginx when configuration changes
-# service { 'nginx': }
 # Installs a Nginx server 
 exec {'install':
   provider => shell,
