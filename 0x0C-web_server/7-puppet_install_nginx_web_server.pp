ca74b3db7f590bb3c630fdd3eefd1d22b205a617
Sarahgad <sarahgad91@gmail.com>
2023-11-29 00:26:04 +0200

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -13,10 +13,9 @@ file { '/var/www/html/index.nginx-debian.html':
 
 # Configure nginx with the redirect
 exec { 'configure_nginx':
-  command => '/bin/bash -c sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+  command => '/bin/sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
   require => Package['nginx'],
   notify  => Service['nginx'],
-  return  => [0,1]
 }
 
 # Restart nginx when configuration changes
