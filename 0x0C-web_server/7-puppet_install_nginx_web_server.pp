7329f8ce79b3c8174876b925fba22fa67932c383
Sarahgad <sarahgad91@gmail.com>
2023-11-29 00:51:23 +0200

    8

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,27 @@
-# Set resource defaults
-Package { ensure => 'present' }
-File { ensure => 'present', owner => 'ubuntu', group => 'ubuntu', mode => '0644' }
-Service { ensure => 'running', enable => true }
+# # Set resource defaults
+# Package { ensure => 'present' }
+# File { ensure => 'present', owner => 'ubuntu', group => 'ubuntu', mode => '0644' }
+# Service { ensure => 'running', enable => true }
 
-# Install nginx package
-package { 'nginx': }
+# # Install nginx package
+# package { 'nginx': }
 
-# Create the index file with dynamic content
-file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello, World!',
+# # Create the index file with dynamic content
+# file { '/var/www/html/index.nginx-debian.html':
+#   content => 'Hello, World!',
+# }
+
+# # Configure nginx with the redirect
+# exec { 'configure_nginx':
+#   command => '/bin/sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+#   require => Package['nginx'],
+#   notify  => Service['nginx'],
+# }
+
+# # Restart nginx when configuration changes
+# service { 'nginx': }
+# Installs a Nginx server 
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:/\/\www.youtube.com/\watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
-
-# Configure nginx with the redirect
-exec { 'configure_nginx':
-  command => '/bin/sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Restart nginx when configuration changes
-service { 'nginx': }
