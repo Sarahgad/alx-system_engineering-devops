70797d3ca6469b3d44c9ba65996e0a0a056c5076
Sarahgad <sarahgad91@gmail.com>
2023-11-29 00:24:05 +0200

    7777

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,6 +16,7 @@ exec { 'configure_nginx':
   command => '/bin/bash -c sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
   require => Package['nginx'],
   notify  => Service['nginx'],
+  return  => [0,1]
 }
 
 # Restart nginx when configuration changes
