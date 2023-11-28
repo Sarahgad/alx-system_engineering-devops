5b38b1bce09124a9a2f140adb663541610c6d00e
Sarahgad <sarahgad91@gmail.com>
2023-11-28 23:59:41 +0200

    7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+#install nginx
+package {'nginx':
+ensure   => 'present',
+provider => 'apt-get'
+}
+#html file
+file {'hello world':
+path    => 'root/var/www/html/index.html',
+user    => 'root',
+mood    => '0755',
+content => 'Hello World!',
+}
+
+#config nginx
+exec {'redirect':
+require  => 'nginx',
+command  => 'sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+provider => 'shell',
+
+}
+#redirect me
