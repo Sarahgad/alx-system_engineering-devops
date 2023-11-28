8f650625419789b1d691556beb2600170a173706
Sarahgad <sarahgad91@gmail.com>
2023-11-29 00:19:16 +0200

    7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,22 @@
-#install nginx
-package {'nginx':
-  ensure   => 'present',
-}
-#install nginx
-exec {'install':
-  command  => 'sudo apt-get update -y; sudo apt-get install nginx -y',
-  provider => 'shell'
-}
-#creat file
-exec {'hello world':
-  command  => 'echo -e "Hello World!" | dd status=none of=/var/www/html/index.nginx-debian.html',
-  provider => 'shell'
-}
-#config nginx
-exec {'redirect':
-  require  => 'nginx',
-  command  => 'sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
-  provider => 'shell',
+# Set resource defaults
+Package { ensure => 'present' }
+File { ensure => 'present', owner => 'ubuntu', group => 'ubuntu', mode => '0644' }
+Service { ensure => 'running', enable => true }
+
+# Install nginx package
+package { 'nginx': }
+
+# Create the index file with dynamic content
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello, World!',
 }
 
-#run
-exec {'run':
-command  => 'sudo service nginx restart',
-provider => 'shell'
+# Configure nginx with the redirect
+exec { 'configure_nginx':
+  command => 'sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
+
+# Restart nginx when configuration changes
+service { 'nginx': }
