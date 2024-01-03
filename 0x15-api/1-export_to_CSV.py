ea801109857c08eec9c62bc3df7e066d12a3fbe7
Sarahgad <sarahgad91@gmail.com>
2024-01-03 23:36:55 +0200

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,10 +13,10 @@ from sys import argv
 if __name__ == '__main__':
     # 2) Fetching --> Get reques for api [Object response]
     user_id = argv[1]
-    url = 'https://jsonplaceholder.typicode.com'
-    user = get(url + '/users/{}'.format(user_id)).json()
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = get(url + 'users/{}'.format(user_id)).json()
     username = user.get('username')
-    todos = get(url + '/todos', params={"userId": user_id}).json()
+    todos = get(url + 'todos', params={"userId": user_id}).json()
 
     with open(user_id + '.cvs', 'w', newline="") as file:
         writ = csv.writer(file, quoting=csv.QUOTE_ALL)
