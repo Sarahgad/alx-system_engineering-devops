009f0dc1614db87a22166d01fa0cd979cbc25e34
Sarahgad <sarahgad91@gmail.com>
2024-01-03 23:34:40 +0200

    1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A given employee ID, returns information
+about his/her TODO list progress.
+"""
+
+
+# 1) Import imp modules
+import csv
+import json
+from requests import get
+from sys import argv
+if __name__ == '__main__':
+    # 2) Fetching --> Get reques for api [Object response]
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = get(url + '/users/{}'.format(user_id)).json()
+    username = user.get('username')
+    todos = get(url + '/todos', params={"userId": user_id}).json()
+
+    with open(user_id + '.cvs', 'w', newline="") as file:
+        writ = csv.writer(file, quoting=csv.QUOTE_ALL)
+        [writ.writerow(
+            [user_id, username, t.get('completed'), t.get('title')]
+            ) for t in todos]
