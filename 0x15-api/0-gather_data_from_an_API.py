e4f5e89899b81a93a4fda4182f9d07f383f4119d
Sarahgad <sarahgad91@gmail.com>
2024-01-03 22:55:33 +0200

    0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+A given employee ID, returns information
+about his/her TODO list progress.
+"""
+
+
+# 1) Import imp modules
+from sys import argv
+from requests import get
+import json
+
+# 2) Fetching --> Get reques for api [Object response]
+response_user = get('https://jsonplaceholder.typicode.com/users')
+response_todos = get('https://jsonplaceholder.typicode.com/todos')
+
+# 3) Json file [deserialization from response]
+user_data = response_user.json()
+user_todos = response_todos.json()
+done = 0
+list_done = []
+# 4) Task:
+
+user_id = argv[1]
+
+for user in user_data:
+    if user['id'] == int(user_id):
+        user_name = user['name']
+        break
+
+for task in user_todos:
+    if task['userId'] == int(user_id):
+        if task['completed'] == 1:
+            list_done.append(task['title'])
+            done += 1
+
+
+print(f"Employee {user['name']} is done with tasks({done}/20):")
+
+for task in list_done:
+    print(f"\t {task}")
